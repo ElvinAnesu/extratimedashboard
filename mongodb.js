c334d548e742d685380190c56d1bff7e975632ba
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-07-16 00:52:27 +0200

    removed environment variables

--- mongodb.js
+++ mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectdb = async() => { 
     try{ 
         if(mongoose.connection.readyState === 0){
-            await mongoose.connect(process.env.MONGODB_URI);
+            await mongoose.connect("mongodb+srv://elvinlite:iUJTfGm8ForEueYo@cluster0.9pd6kmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
             console.log("db reconnected");
         }
         console.log("reconnected");
