69f2002d8272f21a72cdbd32417d4285651a895e
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-07-18 09:23:07 +0200

    added sales total calculations

--- app/dashboard/airtimeservice/page.jsx
+++ app/dashboard/airtimeservice/page.jsx
@@ -13,31 +13,7 @@ export default function AirtimeService(){
     return(
         <div className="w-full h-full flex flex-col gap-4 bg-white">
             <Header title="Econet Airtime" />
-            <div className="flex w-full items-center justify-end">
-                <div className="flex">
-                    <button className={`${showTab==="balances" ? "bg-blue-900 text-white":"bg-gray-200 text-black hover:bg-blue-200"} text-sm font-semibold p-2`}
-                        onClick={() => setShowtab("balances")}>
-                        Balances
-                    </button>
-                    <button className={`${showTab==="transactions" ? "bg-blue-900 text-white":"bg-gray-200 text-black hover:bg-blue-200"} text-sm font-semibold p-2`}
-                        onClick={() => setShowtab("transactions")}>
-                        Transactions
-                    </button>
-                    <button className={`${showTab==="execute" ? "bg-blue-900 text-white":"bg-gray-200 text-black hover:bg-blue-200"} text-sm font-semibold p-2`}
-                        onClick={() => setShowtab("execute")}>
-                        Execute
-                    </button>
-                    <button className={`${showTab==="loadvouchers" ? "bg-blue-900 text-white":"bg-gray-200 text-black hover:bg-blue-200"} text-sm font-semibold p-2`}
-                        onClick={() => setShowtab("loadvouchers")}>
-                        Load Vouchers
-                    </button>
-                </div>
-            </div>
-            {showTab === "balances"? 
-                <AirtimeBalancesTable /> : showTab === "transactions"? 
-                <TransactionTable /> : showTab === "loadvouchers" ? 
-                <LoadAirtimeVouuchersForm /> :showTab === "execute"?
-                <BuyAirtimeForm /> : ""}
+            <TransactionTable />
         </div>
     )
 }
--- components/tables/transactionstable.jsx
+++ components/tables/transactionstable.jsx
@@ -1,10 +1,15 @@
 "use client"
 import { ReloadIcon } from "@radix-ui/react-icons"
 import { useEffect, useState } from "react"
+import moment from "moment"
+import DashboardCard from "../cards/dashboardcards"
 
 export default function TransactionTable(){
 
     const [transactions, setTransactions] = useState([])
+    const [totalSales, setTotalSales] = useState(0)
+    const [pendingSales, setPendingSales] = useState(0)
+    const [clearedSales, setClearedSales] = useState(0)
     const [errorMsg, setErrormsg] = useState()
 
     const getTransactions = async() => {
@@ -27,8 +32,36 @@ export default function TransactionTable(){
         getTransactions()
     },[])
 
+    useEffect(()=>{
+        const calculateSales = () => {
+            var _clearedsales = 0;
+            var _pendingsales = 0;
+            transactions.forEach(transaction => {
+                if(transaction.cleared){
+                    _clearedsales = _clearedsales + transaction.extras.amount
+                }else{
+                    _pendingsales = _pendingsales + transaction.extras.amount
+                }
+            });
+            setClearedSales(_clearedsales)
+            setPendingSales(_pendingsales)
+            setTotalSales(_clearedsales+_pendingsales)
+        }
+        calculateSales()
+    }, [transactions])
     return(
         <div className="w-full h-full flex flex-col">
+            <div className="flex w-full justify-end gap-8 mb-4">
+                <DashboardCard 
+                        value= {`USD${pendingSales.toFixed(2) }`}
+                        product={"Pending Sales"}/>
+                    <DashboardCard 
+                        value={`USD${clearedSales.toFixed(2)}`}
+                        product={"Cleared Sales"}/>
+                    <DashboardCard
+                        value={`USD${totalSales.toFixed(2)}`}
+                        product={"Total Sales"}/>
+            </div>
             <div className="w-full flex items-center justify-between bg-blue-900 px-4 py-1">
                 <h1 className="text-sm text-white font-semibold">Transactions</h1>
                 <button className="text-sm text-white flex items-center gap-2"
@@ -44,16 +77,16 @@ export default function TransactionTable(){
                         <td className="px-4">Currency</td>
                         <td className="px-4">Amount</td>
                         <td className="px-4">Executed by</td>
-                        <td className="px-4">Status</td>
+                        <td className="px-4">Cleared</td>
                         <td className="px-4">Time</td>
                     </tr>
                     {transactions.map((transaction, i)=> (
                         <tr className="bg-gray-200 font-semibold text-xs py-1 border-b border-white" key={i}>
                             <td className="px-4">{transaction.transaction}</td>
                             <td className="px-4">{transaction.currency}</td>
-                            <td className="px-4">{transaction.amount}</td>
-                            <td className="px-4">{transaction.userid}</td>
-                            <td className={`px-4 ${(transaction.issuccessful) === true ? "text-green-600" : "text-red-600"}`}>{(transaction.issuccessful) === true ? "Success" : "Failed"}</td>
+                            <td className="px-4">{transaction.amount}</td>  
+                            <td className="px-4">{transaction.username}</td>
+                            <td className={`px-4 ${(transaction.cleared) === true ? "text-green-600" : "text-amber-600"}`}>{(transaction.cleared) === true ? "Cleared" : "Pending"}</td>
                             <td className="px-4">{transaction.createdAt}</td>
                         </tr>
                     ))}
