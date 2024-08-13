635190b008405632bfce8c298c39dc75a5c1e788
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-08-13 12:23:37 +0200

    added loader on supervisor agents

--- components/tables/supervisoragentstable.jsx
+++ components/tables/supervisoragentstable.jsx
@@ -1,30 +1,22 @@
 // components/AgentsTable.js
 "use client";
-import { EyeOpenIcon, TrashIcon } from "@radix-ui/react-icons";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import ConfirmDelete from "../dialogs/confirmdelete";
-import AlertDialog from "../dialogs/alertdialog";
-
 const PAGE_SIZE = 10;
 
 export default function SupervisorAgentsTable({supervisor}) {
-  const router = useRouter();
 
-  const [showdialog, setShowdialog] = useState(false);
-  const [showDeletedialog, setShowdeletedialog] = useState(false);
+  
   const [users, setUsers] = useState([]);
   const [salesData, setSalesData] = useState({});
-  const [error, setError] = useState();
-  const [deleteuser, setDeleteuser] = useState();
-  const [deletedialogtitle, setDeletedialogtitle] = useState();
-  const [deletedialogmsg, setDeletedialogmsg] = useState();
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
+  const [isfetching, setIsfetching] = useState(false)
 
 
 
   const getAgents= async (page = 1) => {
+    setIsfetching(true)
+    console.log(isfetching)
     const res = await fetch("/api/supervisors/supervisoragents", {
       method: "POST",
       headers: { "Content-type": "application/json" },
@@ -41,11 +33,12 @@ export default function SupervisorAgentsTable({supervisor}) {
       setUsers(data.users);
       setTotal(data.total);
       // Fetch sales data for each user
-      data.users.forEach(user => {
+      await data.users.forEach(user => {
         getTotalSalesForUser(user._id);
       });
+      setIsfetching(false)
     } else {
-      setError("Error fetching users");
+      setIsfetching(false)
     }
   };
 
@@ -70,10 +63,7 @@ export default function SupervisorAgentsTable({supervisor}) {
     }
   };
 
-  const onOk = () => {
-    setShowdeletedialog(false);
-    window.location.reload();
-  };
+
 
   useEffect(() => {
     getAgents(page);
@@ -92,56 +82,52 @@ export default function SupervisorAgentsTable({supervisor}) {
   };
 
   return (
-    <div className="min-w-full max-h-full overflow-hidden text-sm">
-      <table className="w-full">
-        <tbody>
-          <tr className="bg-gray-200 font-semibold">
-            <td className="border border-white px-1">#</td>
-            <td className="border border-white px-1">Agent name</td>
-            <td className="border border-white px-1">Phone Number</td>
-            <td className="border border-white px-1">Location</td>
-            <td className="border border-white px-1">Machine Number</td>
-            <td className="border border-white px-1">Cash in Hand</td>
-            <td className="border border-white px-1">Total Sales</td>
-          </tr>
-          {users? users.map((user, i) => (
-            <tr className="" key={i}>
-              <td className="border-b border px-1">{(page - 1) * PAGE_SIZE + i + 1}</td>
-              <td className="border-b border px-1">{`${user.firstname} ${user.surname}`}</td>
-              <td className="border-b border px-1">{user.phonenumber}</td>
-              <td className="border-b border px-1">{user.location}</td>
-              <td className="border-b border px-1"></td>
-              <td className="border-b border px-1">
-                {salesData[user._id] ? salesData[user._id].pending : "Loading..."}
-              </td>
-              <td className="border-b border px-1">
-                {salesData[user._id] ? salesData[user._id].cleared : "Loading..."}
-              </td>
-            </tr>
-          )) : ""}
-        </tbody>
-      </table>
-
-      <div className="flex justify-between items-center mt-4">
-        <button onClick={handlePreviousPage} disabled={page === 1} className="px-4 py-2 bg-blue-500 text-white">
-          Previous
-        </button>
-        <span className="text-sm">Page {page}</span>
-        <button onClick={handleNextPage} disabled={page * PAGE_SIZE >= total} className="px-4 py-2 bg-blue-500 text-white">
-          Next
-        </button>
-      </div>
-
-      {showdialog && (
-        <ConfirmDelete
-          title="Delete Administrator!"
-          message="Are you sure you want to delete this administrator?"
-          onCancel={() => setShowdialog(false)}
-          onConfirm={deleteUser}
-        />
+    <div className="flex w-full h-full">
+      {isfetching? ( 
+        <div className="w-full flex items-center justify-center p-8"> 
+          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+        </div>
+      ):( 
+        <div className="min-w-full max-h-full overflow-hidden text-sm">
+          <table className="w-full">
+            <tbody>
+              <tr className="bg-gray-200 font-semibold">
+                <td className="border border-white px-1">#</td>
+                <td className="border border-white px-1">Agent name</td>
+                <td className="border border-white px-1">Phone Numb</td>
+                <td className="border border-white px-1">Location</td>
+                <td className="border border-white px-1">Machine Number</td>
+                <td className="border border-white px-1">Cash in Hand</td>
+                <td className="border border-white px-1">Total Sales</td>
+              </tr>
+              {users.map((user, i) => (
+                <tr className="" key={i}>
+                  <td className="border-b border px-1">{(page - 1) * PAGE_SIZE + i + 1}</td>
+                  <td className="border-b border px-1">{`${user.firstname} ${user.surname}`}</td>
+                  <td className="border-b border px-1">{user.phonenumber}</td>
+                  <td className="border-b border px-1">{user.location}</td>
+                  <td className="border-b border px-1"></td>
+                  <td className="border-b border px-1">
+                    {salesData[user._id] ? salesData[user._id].pending : "Loading..."}
+                  </td>
+                  <td className="border-b border px-1">
+                    {salesData[user._id] ? salesData[user._id].cleared : "Loading..."}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-between items-center mt-4">
+            <button onClick={handlePreviousPage} disabled={page === 1} className="px-4 py-2 bg-blue-500 text-white">
+              Previous
+            </button>
+            <span className="text-sm">Page {page}</span>
+            <button onClick={handleNextPage} disabled={page * PAGE_SIZE >= total} className="px-4 py-2 bg-blue-500 text-white">
+              Next
+            </button>
+          </div>
+        </div>
       )}
-
-      {showDeletedialog && <AlertDialog title={deletedialogtitle} message={deletedialogmsg} onOk={onOk} />}
     </div>
-  );
+  )
 }
