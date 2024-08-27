65bc25d841421e24c2dd1fc3117ea2eeefe2379b
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-08-27 14:16:02 +0200

    added return functionality

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,12 @@
+"use client"
 import { ArrowLeftIcon } from "@radix-ui/react-icons"
+import { useRouter } from "next/navigation"
 
-export default function Header({title, onReturn}){ 
+export default function Header({title}){ 
+    const router = useRouter()
     return( 
         <div className="flex items-center p-2 h-9 bg-blue-900 text-white rounded shadow w-full gap-2">
-            <button onClick={onReturn}>
+            <button onClick={()=> router.back()}>
                 <ArrowLeftIcon />
             </button>
             <h1 className="font-semibold">{title}</h1>
